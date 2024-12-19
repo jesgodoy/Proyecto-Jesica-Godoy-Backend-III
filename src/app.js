82d6a14e4f315591d0abde3bf396a9303cb13deb
jesgodoy <jesicagodoy7@gmail.com>
2024-12-19 17:58:36 -0300

    gitignore .env

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -8,11 +9,21 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from './routes/mocks.router.js'; // Asegúrate de importar el router de mocks
 
+
+dotenv.config(); 
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 // Registrar las rutas
 app.use('/api/users', usersRouter);
@@ -20,11 +31,6 @@ app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter); // Aquí registramos las rutas de mocks
-
-// Conexión a la base de datos
-mongoose.connect('mongodb+srv://jesigodoyprogramacion:Coderbackend@cluster0.4rbfc.mongodb.net/Adoptme?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log("Conectado a MongoDB"))
-    .catch(error => console.error("Error de conexión a MongoDB:", error));
 
 app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`);
